486a690650f8e53a5a588b1cdcef9d591a49feef
tapdevops <44764573+tapdevops@users.noreply.github.com>
2018-11-22 14:37:40 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	//
-	app_port: process.env.PORT || 3003,
-	app_name: 'Microservices Hectare Statement',
+	app_port: process.env.PORT || 3001,
+	app_name: 'Microservices Hectare Statement test',
 	url: {
 		sync_list: 'http://149.129.242.205:3001/sync/list'
 		//sync_list: 'http://localhost:3001/sync/list'
